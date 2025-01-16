aeee3691bea7207fe0233aaadd82f4d60d5327e2
sean <seanteh2001@gmail.com>
2025-01-16 21:46:39 +0800

    add product card component, added main screen to view products

--- src/admin/products/AdminViewProduct.tsx
+++ src/admin/products/AdminViewProduct.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
+import { db } from "@/firebase";
 import ProductCard from "@/components/ProductCard";
+import { collection, query, getDocs, DocumentData } from "firebase/firestore";
+import { Stack } from "@chakra-ui/react";
 
 const AdminViewProduct = () => {
+  const [products, setProducts] = useState<DocumentData>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const q = query(collection(db, "products"));
+        const querySnapshot = await getDocs(q);
+        const productsData: DocumentData = [];
+        querySnapshot.forEach((doc) => {
+          productsData.push({ id: doc.id, ...doc.data() });
+        });
+        setProducts(productsData);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    console.log("inside useEffect");
+    fetchProducts();
+  }, []);
+
   return (
-    <ProductCard />
+    <div>
+      <Stack>
+        {products.map((product: DocumentData) => (
+          <ProductCard key={product.id} id={product.id} name={product.name} quantity={product.quantity} value={product.value} image={product.image}/>
+        ))}
+      </Stack>
+    </div>
   );
 };
 
