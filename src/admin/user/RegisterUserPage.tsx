657ae472f88099f67a591e8b8508355008dc29b8
sean <seanteh2001@gmail.com>
2025-01-16 16:12:57 +0800

    change tab space from 4 to 2

--- src/admin/user/RegisterUserPage.tsx
+++ src/admin/user/RegisterUserPage.tsx
@@ -5,78 +5,84 @@ import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { usernameToEmail } from "../../utils/UsernameToEmail";
 
 export default function RegisterUserPage() {
-    const [name, setName] = useState("");
-    const [dob, setDob] = useState("");
-    const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [dob, setDob] = useState("");
+  const [image, setImage] = useState("");
 
-    const navigate = useNavigate();
-    
-    const getDate = (date: Date) => {
-        return date.toISOString().split('T')[0];
-    }
+  const navigate = useNavigate();
 
-    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value);
-    };
+  const getDate = (date: Date) => {
+    return date.toISOString().split("T")[0];
+  };
 
-    const handleDobChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value);
-        const newDob = new Date(e.target.value);
-        setDob(getDate(newDob));
-    };
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
 
-    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-            const file = e.target.files[0];
-            const reader = new FileReader();
-            reader.onload = () => {
-                if (reader.readyState === 2) {
-                    setImage(reader.result as string);
-                }
-            };
-            reader.readAsDataURL(file);
+  const handleDobChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    const newDob = new Date(e.target.value);
+    setDob(getDate(newDob));
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          setImage(reader.result as string);
         }
-    };
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
-    const handleRegister = (e: React.FormEvent) => {
-        e.preventDefault();
-        const newUser: User = {
-            id: Math.random().toString(36),
-            name: name,
-            email: usernameToEmail(name, true),
-            dob: dob,
-            image: image
-        };
-        const auth = getAuth();
-        createUserWithEmailAndPassword(auth, newUser.email, "password").then(() => {
-            // Signed up 
-            alert("Resident added successfully");
-          })
-          .catch((error) => {
-            alert(error.message);
-            console.log(error.code);
-          });
+  const handleRegister = (e: React.FormEvent) => {
+    e.preventDefault();
+    const newUser: User = {
+      id: Math.random().toString(36),
+      name: name,
+      email: usernameToEmail(name, true),
+      dob: dob,
+      image: image,
     };
-    return (
-        <div>
-            <h1>Register New Resident</h1>
-            <form onSubmit={handleRegister}>
-                <label>Name</label>
-                <input value={name} onChange={handleNameChange} />
-                <br />
-                <label>Date of Birth</label>
-                <input type="date" value={dob} onChange={handleDobChange}/>
-                <br />
-                <label>Image of Resident</label>
-                <input type="file" name="image" value={image} onChange={handleImageChange}/>
-                <br />
-                <button type="submit">Register</button>
-            </form>
-            <div>
-                <button onClick={() => navigate("/admin/user/")}>
-                    Back to User Page
-                </button>
-            </div>
-        </div>
-    )
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, newUser.email, "password")
+      .then(() => {
+        // Signed up
+        alert("Resident added successfully");
+      })
+      .catch((error) => {
+        alert(error.message);
+        console.log(error.code);
+      });
+  };
+  return (
+    <div>
+      <h1>Register New Resident</h1>
+      <form onSubmit={handleRegister}>
+        <label>Name</label>
+        <input value={name} onChange={handleNameChange} />
+        <br />
+        <label>Date of Birth</label>
+        <input type="date" value={dob} onChange={handleDobChange} />
+        <br />
+        <label>Image of Resident</label>
+        <input
+          type="file"
+          name="image"
+          value={image}
+          onChange={handleImageChange}
+        />
+        <br />
+        <button type="submit">Register</button>
+      </form>
+      <div>
+        <button onClick={() => navigate("/admin/user/")}>
+          Back to User Page
+        </button>
+      </div>
+    </div>
+  );
 }
